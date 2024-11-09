09fb109d56bc3d842b3f938fd95d5932978a52d0
Michael Böckli <michael.boeckli@gmail.com>
2024-11-09 11:33:59 +0100

    Send transaction successfully

--- go.mod
+++ go.mod
@@ -2,11 +2,11 @@ module node-analysis
 
 go 1.23.0
 
+replace github.com/btcsuite/btcd => github.com/boecklim/btcd v0.24.3
+
 require (
-	github.com/bitcoin-sv/go-sdk v1.1.14
-	github.com/bitcoinsv/bsvutil v0.0.0-20181216182056-1d77cf353ea9
 	github.com/btcsuite/btcd v0.24.2
-	github.com/btcsuite/btcd/btcec/v2 v2.1.3
+	github.com/btcsuite/btcd/btcec/v2 v2.3.4
 	github.com/btcsuite/btcd/btcutil v1.1.5
 	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0
 	github.com/go-zeromq/zmq4 v0.13.0
@@ -14,15 +14,14 @@ require (
 )
 
 require (
-	github.com/bitcoinsv/bsvd v0.0.0-20190609155523-4c29707f7173 // indirect
 	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f // indirect
 	github.com/btcsuite/go-socks v0.0.0-20170105172521-4720035b7bfd // indirect
 	github.com/btcsuite/websocket v0.0.0-20150119174127-31079b680792 // indirect
 	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/go-zeromq/goczmq/v4 v4.2.2 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 )
--- utils/tx_creator.go
+++ utils/tx_creator.go
@@ -1,51 +1,46 @@
 package utils
 
 import (
+	"encoding/hex"
+
 	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcjson"
 	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
 )
 
-func PayToAddress(addr btcutil.Address, originTx *btcutil.Tx, privKey *btcec.PrivateKey) (*wire.MsgTx, error) {
+func PayToAddress(address btcutil.Address, hash *chainhash.Hash, txOut *btcjson.GetTxOutResult, privKey *btcec.PrivateKey) (*wire.MsgTx, error) {
 	tx := wire.NewMsgTx(wire.TxVersion)
-
-	hash := originTx.Hash()
 
 	prevOut := wire.NewOutPoint(hash, 0)
 	input := wire.NewTxIn(prevOut, nil, nil)
 	tx.AddTxIn(input)
 
-	pkScript, err := txscript.PayToAddrScript(addr)
+	pkScript, err := txscript.PayToAddrScript(address)
 	if err != nil {
 		return nil, err
 	}
+
+	valueSat := int64(txOut.Value * 1e8)
 	tx.AddTxOut(wire.NewTxOut(1000, []byte(pkScript)))
-
-	// toAddr, err := btcutil.DecodeAddress(utxo.Address, &chaincfg.RegressionNetParams)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	pkScript2, err := txscript.PayToAddrScript(addr)
-	if err != nil {
-		return nil, err
-	}
-
-	tx.AddTxOut(wire.NewTxOut(int64(originTx.MsgTx().TxOut[0].Value-1500), []byte(pkScript2)))
+	tx.AddTxOut(wire.NewTxOut(valueSat-1500, []byte(pkScript)))
 
 	lookupKey := func(a btcutil.Address) (*btcec.PrivateKey, bool, error) {
 		return privKey, true, nil
 	}
 
-	pkScriptOrig := originTx.MsgTx().TxOut[0].PkScript
+	pkScriptOrig, err := hex.DecodeString(txOut.ScriptPubKey.Hex)
+	if err != nil {
+		return nil, err
+	}
 
 	sigScript, err := txscript.SignTxOutput(&chaincfg.MainNetParams,
 		tx, 0, pkScriptOrig, txscript.SigHashAll,
 		txscript.KeyClosure(lookupKey), nil, nil)
 	if err != nil {
-
 		return nil, err
 	}
 	tx.TxIn[0].SignatureScript = sigScript
