fcb108656ead938363198a45fd84e39a0743b4c2
Michael Böckli <michael.boeckli@gmail.com>
2024-11-16 16:21:43 +0100

    Virtual machine, outputs, private key

--- infra/main.tf
+++ infra/main.tf
@@ -62,6 +62,18 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "AllowCidrBlockCustom18444Inbound"
+    priority                   = 800
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "18444"
+    source_address_prefix      = "10.1.1.0/24"
+    destination_address_prefix = "*"
+  }
 }
 
 # Connect the security group to the network interface
@@ -89,44 +101,38 @@ resource "azurerm_network_interface_security_group_association" "example" {
 #   account_replication_type = "LRS"
 # }
 
-# # Create (and display) an SSH key
-# resource "tls_private_key" "example_ssh" {
-#   algorithm = "RSA"
-#   rsa_bits  = 4096
-# }
+# Create (and display) an SSH key
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
-# # Create virtual machine
-# resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
-#   name                  = "myVM"
-#   location              = azurerm_resource_group.rg.location
-#   resource_group_name   = azurerm_resource_group.rg.name
-#   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
-#   size                  = "Standard_DS1_v2"
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
+  name                            = "myVM"
+  location                        = azurerm_resource_group.rg.location
+  resource_group_name             = azurerm_resource_group.rg.name
+  network_interface_ids           = [azurerm_network_interface.my_terraform_nic.id]
+  size                            = "Standard_B1s"
+  computer_name                   = "myvm"
+  admin_username                  = "azureuser"
+  disable_password_authentication = true
 
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.example_ssh.public_key_openssh
+  }
 
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+    name                 = "myOsDisk"
+  }
 
-#   source_image_reference {
-#     publisher = "Canonical"
-#     offer     = "0001-com-ubuntu-server-jammy"
-#     sku       = "22_04-lts-gen2"
-#     version   = "latest"
-#   }
-
-#   computer_name                   = "myvm"
-#   admin_username                  = "azureuser"
-#   disable_password_authentication = true
-
-#   admin_ssh_key {
-#     username   = "azureuser"
-#     public_key = tls_private_key.example_ssh.public_key_openssh
-#   }
-#   os_disk {
-#     caching              = "ReadWrite"
-#     storage_account_type = "Premium_LRS"
-#     name                 = "myOsDisk"
-#   }
-
-#   boot_diagnostics {
-#     storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
-#   }
-# }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
