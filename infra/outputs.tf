02c90d1f0a638d7fa7b2e30116530c1107f372eb
Michael Böckli <michael.boeckli@gmail.com>
2024-11-16 13:39:07 +0100

    Create virtual network, resource group, network interface, public ip, network security group

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,14 +2,11 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "virtual_network_name" {
-  value = azurerm_virtual_network.test.name
-}
+# output "public_ip_address" {
+#   value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
+# }
 
-output "subnet_name" {
-  value = azurerm_subnet.test.name
-}
-
-output "linux_virtual_machine_names" {
-  value = [for s in azurerm_linux_virtual_machine.test : s.name[*]]
-}
+# output "tls_private_key" {
+#   value     = tls_private_key.example_ssh.private_key_pem
+#   sensitive = true
+# }
--- infra/providers.tf
+++ infra/providers.tf
@@ -14,18 +14,18 @@
 
 terraform {
   required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = "~>1.5"
-    }
+    # azapi = {
+    #   source  = "azure/azapi"
+    #   version = "~>1.5"
+    # }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.75.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
+    # random = {
+    #   source  = "hashicorp/random"
+    #   version = "~>3.0"
+    # }
   }
 
   required_version = ">= 1.5.7"
