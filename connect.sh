c76c38a1e806a4b9a4fd1064d8f4ed236fe32b4b
Michael Böckli <michael.boeckli@gmail.com>
2024-12-08 11:24:17 +0100

    Update connect script

--- connect.sh
+++ connect.sh
@@ -2,8 +2,6 @@
 
 VM_NR=$1
 
-echo "connecting to VM ${VM_NR}"
-
 BASTION_HOST_NAME=$(terraform -chdir=infra output -json bastion_host_name | jq -r)
 
 RESOURCE_GROUP_NAME=$(terraform -chdir=infra output -json resource_group_name | jq -r)
@@ -11,5 +9,7 @@ RESOURCE_GROUP_NAME=$(terraform -chdir=infra output -json resource_group_name | 
 readarray -t VM_RESOURCE_IDS < <(terraform -chdir=infra output -json vm_resource_ids | jq -r '.[].[]')
 
 chmod 400 ./infra/private_keys/cloudtls.pem
+
+echo "connecting to VM ${VM_RESOURCE_IDS[VM_NR]}"
 
 az network bastion ssh --name ${BASTION_HOST_NAME} --resource-group $RESOURCE_GROUP_NAME --target-resource-id ${VM_RESOURCE_IDS[VM_NR]} --auth-type "ssh-key" --username azureuser --ssh-key ./infra/private_keys/cloudtls.pem
